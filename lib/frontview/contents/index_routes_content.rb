cf119945d4cc66cfee1764299498ba36982e49c1
elibiz443 <ellyambet10@gmail.com>
2021-09-20 07:06:34 +0300

    Updated index links

--- lib/frontview/contents/index_routes_content.rb
+++ lib/frontview/contents/index_routes_content.rb
@@ -47,21 +47,21 @@ module Frontview
   </div>
 </div>
 
-<div class="row content" id="#intro">
+<div class="row content" id="intro">
   <h1 class="sec01">Intoduction</h1>
   <p class="par01">
     At FrontView, we develop innovative & creative products & services that provide total communication & information solutions. Among a plethora of services, web design and development, tailor made applications, ERPs, CRMs, e-commerce solutions, business-to-business applications, business-to-client applications, managed hosting and internet portal management are few that we offer.
   </p>
 </div>
 
-<div class="row content" id="#about">
+<div class="row content" id="about">
   <h1 class="sec01">About Us</h1>
   <p class="par01">
     We offer software solutions to clients around the globe. Those we\'ve worked with bear testimony to the quality of our work. As a leader in technology exploring, FrontView is committed to exporting quality software worldwide. The general purpose of FrontView is to develop and promote advanced information technologies for multi-user operation.FrontView\'s business philosophy is to assure the highest quality product, total client satisfaction, timely delivery of solutions and the best quality/price ratio found in the industry. Our emphasis is on offering a high degree of product user friendliness through a positive, creative and focused company staff.
   </p>
 </div>
 
-<div class="row content" id="#service">
+<div class="row content" id="service">
   <h1 class="sec01">Our Services</h1>
   <h6 class="sec01">
     Web Design & Development ||
@@ -73,7 +73,7 @@ module Frontview
   </p>
 </div>
 
-<div class="row bottom-content" id="#contact">
+<div class="row bottom-content" id="contact">
   <h1 class="sec01">Contact Us</h1>
   <p class="text-center"><i>Send us a message</i></p>
   <form>
